c31c79c928f61a8f0a83421d34b97abb11df0a6f
lixiang <lixiang99410@126.com>
2022-05-17 13:28:03 +0800

    rm debug shell scripts

--- trace_mcf.sh
+++
@@ -1,6 +0,0 @@
-./scripts/run_spec_with_roi.sh \
-505.mcf_r.5B_s50B_roi2 \
-/mnt/centos00-home/lixiang/spec2017/benchspec/CPU/505.mcf_r/build/build_base_mytest-m64.0001/mcf_r \
-/mnt/centos00-home/lixiang/spec2017/benchspec/CPU/505.mcf_r/data/refrate/input/inp.in \
-/home/lixiang/mem-pattern-recognition/perf-test/roi_mcf_2.txt \
-5000000000 true false false false false false
